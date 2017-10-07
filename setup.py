1a532dd86402deba17c24e58ae59dc2bb80fde64
Bryn M. Reeves <bmr@redhat.com>
2017-10-07 14:23:13 +0100

    build: install boom to /bin/boom
    Signed-off-by: Bryn M. Reeves <bmr@redhat.com>

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 from setuptools import setup, Extension
 
-setup(name='boom',
-      version="0.1",
-      description=("""The Boom Boot Manager."""),
-      author='Bryn M. Reeves',
-      author_email='bmr@redhat.com',
-      url='https://github.com/bmr-cymru/boom',
-      license="GPLv2",
-      test_suite="tests",
-      packages=['boom'],
-     )
+setup(
+    name='boom',
+    version="0.1",
+    description=("""The Boom Boot Manager."""),
+    author='Bryn M. Reeves',
+    author_email='bmr@redhat.com',
+    url='https://github.com/bmr-cymru/boom',
+    license="GPLv2",
+    test_suite="tests",
+    scripts=['bin/boom'],
+    packages=['boom'],
+)
 
 
 # vim: set et ts=4 sw=4 :
