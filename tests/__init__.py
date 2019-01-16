55a8c905d3aade1e0e05a206938b7aca1b6957b1
Bryn M. Reeves <bmr@redhat.com>
2019-01-16 10:07:46 +0000

    tests: add sandbox functions to tests module
    Add simple functions for managing the sandbox directory to the
    main tests module.
    
    Signed-off-by: Bryn M. Reeves <bmr@redhat.com>

--- tests/__init__.py
+++ tests/__init__.py
@@ -13,13 +13,49 @@
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
 from os.path import join, abspath
-from os import geteuid, getegid
+from os import geteuid, getegid, makedirs
+import shutil
+import errno
+
+import boom
 
 # Root of the testing directory
 BOOT_ROOT_TEST = abspath("./tests")
 
 # Location of the temporary sandbox for test data
 SANDBOX_PATH = join(BOOT_ROOT_TEST, "sandbox")
+
+# Test sandbox functions
+
+def rm_sandbox():
+    """Remove the test sandbox at SANDBOX_PATH.
+    """
+    try:
+        shutil.rmtree(SANDBOX_PATH)
+    except OSError as e:
+        if e.errno != errno.ENOENT:
+            raise
+
+
+def mk_sandbox():
+    """Create a new test sandbox at SANDBOX_PATH.
+    """
+    makedirs(SANDBOX_PATH)
+
+
+def reset_sandbox():
+    """Reset the test sandbox at SANDBOX_PATH by removing it and
+        re-creating the directory.
+    """
+    rm_sandbox()
+    mk_sandbox()
+
+def reset_boom_paths():
+    """Reset configurable boom module paths to the default test values.
+    """
+    boom.set_boot_path(BOOT_ROOT_TEST)
+
+# Mock objects
 
 class MockArgs(object):
     """Mock arguments class for testing boom command line infrastructure.
@@ -45,6 +81,8 @@ class MockArgs(object):
     host_profile = ""
     label = ""
 
+
+# Test predicates
 
 def have_root():
     """Return ``True`` if the test suite is running as the root user,
