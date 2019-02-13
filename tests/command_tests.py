61988a2b5ebfe5b917d280c5ff72950e1f0bbb24
Bryn M. Reeves <bmr@redhat.com>
2019-02-13 15:45:28 +0000

    tests: test _delete_cmd() handler with custom report fields
    Signed-off-by: Bryn M. Reeves <bmr@redhat.com>

--- tests/command_tests.py
+++ tests/command_tests.py
@@ -1341,6 +1341,17 @@ class CommandTests(unittest.TestCase):
         r = boom.command._delete_cmd(args, None, opts, None)
         self.assertEqual(r, 0)
 
+    def test__delete_cmd_with_fields(self):
+        """Test the _delete_cmd() handler with a valid entry and report
+            field options string.
+        """
+        args = MockArgs()
+        args.boot_id = "61bcc49"
+        opts = boom.command._report_opts_from_args(args)
+        args.options = "title,boot_id"
+        r = boom.command._delete_cmd(args, None, opts, None)
+        self.assertEqual(r, 0)
+
     def test__delete_cmd_verbose(self):
         """Test the _delete_cmd() handler with a valid entry and
             verbose output.
