1369d5f49b1511acc87fed9382b7b0ed22db3d5e
Bryn M. Reeves <bmr@redhat.com>
2019-02-19 15:42:24 +0000

    boom.command: accept --host-id or <host id> for _edit_host_cmd()
    Signed-off-by: Bryn M. Reeves <bmr@redhat.com>

--- boom/command.py
+++ boom/command.py
@@ -2265,6 +2265,7 @@ def _edit_host_cmd(cmd_args, select, opts, identifier):
         :param select: The ``host_id`` of the profile to edit
         :returns: integer status code returned from ``main()``
     """
+    identifier = identifier or cmd_args.host_id
     if identifier is not None:
         select = Selection(host_id=identifier)
 
