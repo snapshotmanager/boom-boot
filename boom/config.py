45a3e84db9a6db6e3abcf71c542dd8382815bd35
Bryn M. Reeves <bmr@redhat.com>
2019-10-24 12:19:46 +0100

    boom.config: fix docstring typo
    "classes and constants and functions, oh my!"
    
    Signed-off-by: Bryn M. Reeves <bmr@redhat.com>

--- boom/config.py
+++ boom/config.py
@@ -11,7 +11,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program; if not, write to the Free Software Foundation,
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
-"""The ``boom.config`` module defines classes and constants and functions
+"""The ``boom.config`` module defines classes, constants and functions
 for reading and writing persistent (on-disk) configuration for the boom
 library and tools.
 
