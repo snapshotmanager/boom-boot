e4292b341e191cf2e0d89400ee9bf6c19509bd74
Bryn M. Reeves <bmr@redhat.com>
2020-05-11 15:38:53 +0100

    boom: get reference to BoomConfig in boom.command.main()
    Store a reference to the BoomConfig object returned by the call to
    load_boom_config().
    
    Signed-off-by: Bryn M. Reeves <bmr@redhat.com>

--- boom/command.py
+++ boom/command.py
@@ -3111,7 +3111,7 @@ def main(args):
     if cmd_args.config:
         set_boom_config_path(cmd_args.config)
 
-    load_boom_config()
+    bc = load_boom_config()
 
     if not path_exists(get_boom_path()):
         _log_error("Configuration directory '%s' not found." %
