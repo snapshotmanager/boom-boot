a307adf1c1fcba70cfb2d36aa3861050ad1c168d
Bryn M. Reeves <bmr@redhat.com>
2024-06-19 17:28:16 +0100

    tests: fix broken test for config file section name
    The BoomConfig.__str__() method was fixed in 61242ab: fix the test to
    match reality.
    
    Signed-off-by: Bryn M. Reeves <bmr@redhat.com>

--- tests/test_boom.py
+++ tests/test_boom.py
@@ -160,7 +160,7 @@ class BoomTests(unittest.TestCase):
 
     def test_BoomConfig__str__(self):
         bc = boom.BoomConfig(boot_path="/boot", legacy_enable=False)
-        xstr = ('[defaults]\nboot_path = /boot\nboom_path = /boot/boom\n\n'
+        xstr = ('[global]\nboot_path = /boot\nboom_path = /boot/boom\n\n'
                 '[legacy]\nenable = False\nformat = grub1\nsync = True\n\n'
                 '[cache]\nenable = True\nauto_clean = True\n'
                 'cache_path = /boot/boom/cache\n')
