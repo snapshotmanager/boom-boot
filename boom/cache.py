9ffe2fe3d8649146cdefa80cbb4829856651580c
Bryn M. Reeves <bmr@redhat.com>
2024-10-03 15:46:47 +0100

    boom: fix license headers across tree
    Signed-off-by: Bryn M. Reeves <bmr@redhat.com>

--- boom/cache.py
+++ boom/cache.py
@@ -8,7 +8,7 @@
 # modify, copy, or redistribute it subject to the terms and conditions
 # of the GNU General Public License v.2.
 #
-# You should have received a copy of the GNU Lesser General Public License
+# You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software Foundation,
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 """The ``boom.cache`` module defines classes, constants and functions
